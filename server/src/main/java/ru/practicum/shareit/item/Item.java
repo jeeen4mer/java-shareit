0b2cf38b640c20e47c1a8a689d00b8cd575684a1
JeeN4MeR <ibakulev04@gmail.com>
2025-11-27 20:34:14 +0400

    Fixed checks 7

---
+++ server/src/main/java/ru/practicum/shareit/item/Item.java
@@ -0,0 +1,34 @@
+package ru.practicum.shareit.item;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", length = 1000)
+    private String description;
+
+    @Column(name = "available", nullable = false)
+    private Boolean available;
+
+    @Column(name = "owner_id", nullable = false)
+    private Long ownerId;
+
+    @Column(name = "request_id")
+    private Long requestId;
+}
